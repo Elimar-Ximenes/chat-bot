97a642cabd766ce228f126514a81192e3feb634a
Elimar-Ximenes <elimarximenes123@gmail.com>
2023-09-25 14:21:30 -0300

    Subindo Projeto

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 dotenv.config();
 // Configure as variáveis de ambiente com as informações de conexão ao banco de dados
-const DATABASE_URL='mysql://mqj2f1qb55853lidgkk3:pscale_pw_kDHygdrqu8iGE131ch4aDMG5pWFVLDeqtzcyyA5yaxM@aws.connect.psdb.cloud/chat-bot?ssl={"rejectUnauthorized":true}'
+const DATABASE_URL='mysql://jniu6rru7hjdars3ad40:pscale_pw_j3ZfvcBmGpfCJIlH4UisanXipXrnjCo63Zjjiq1ymep@aws.connect.psdb.cloud/chat-bot?ssl={"rejectUnauthorized":true}'
 // Configuração da conexão com o banco de dados
 const connection = mysql.createConnection(DATABASE_URL);
 
